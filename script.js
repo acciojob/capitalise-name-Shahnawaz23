644c340db62a5793f2b51a81da3ef491967c1fce
Shahnawaz Akhtar <58129001+Shahnawaz23@users.noreply.github.com>
2024-12-18 00:11:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", () => {
+    const inputField = document.getElementById("fname");
+    inputField.addEventListener("blur", () => {
+        inputField.value = inputField.value.toUpperCase();
+    });
+});
